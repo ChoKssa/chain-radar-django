424751882c0efa7bc393883b2e832ce911da7581
Alexandre Cathalifaud <alexandre.cathalifaud@epitech.eu>
2025-03-26 23:57:25 +0000

    Remove app_name declaration from urls.py

--- general/urls.py
+++ general/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from . import views
 
-app_name = "myapp"
 urlpatterns = [
     path("", views.index, name="index"),
     path("contact/", views.contact, name="contact"),
