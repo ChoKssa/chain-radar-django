f6bd136a83fdd743eeb7dfbd94db8e6c1cb119f8
Alexandre Cathalifaud <alexandre.cathalifaud@epitech.eu>
2025-04-14 16:32:34 +0100

    Add FollowedCrypto inline to CustomUserAdmin for better user management

--- user/admin.py
+++ user/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from crypto.models import FollowedCrypto
+
+class FollowedCryptoInline(admin.TabularInline):
+    model = FollowedCrypto
+    extra = 0
+    autocomplete_fields = ['crypto']
+    verbose_name = "Followed Crypto"
+    verbose_name_plural = "Followed Cryptos"
+
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
+    inlines = [FollowedCryptoInline]
+
     list_display = (
         'username', 'email', 'first_name', 'last_name',
         'phone_number', 'date_of_birth', 'gender',
